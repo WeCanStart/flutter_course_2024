7995b7b623b3139fba12c4d0c14d6149c22de951
WeCanStart <artemhapilov2004@gmail.com>
2024-10-03 14:57:06 +0300

    no api key lucky me

--- practice_1/bin/practice_1.dart
+++ practice_1/bin/practice_1.dart
@@ -8,10 +8,10 @@ import 'package:practice_1/features/core/presentation/app.dart';
 const String version = '0.0.1';
 
 const String urlOSM = 'https://api.openweathermap.org';
-const String apiKeyOSM = 'f11a8d09666e4acbd56e3ecc1ccbe31b';
+const String apiKeyOSM = '';
 
 const String urlWA = 'https://api.weatherapi.com/v1';
-const String apiKeyWA = 'cf7c0ed87f4940a2909202931242809';
+const String apiKeyWA = '';
 
 void main(List<String> arguments) {
   var app = App(WeatherRepositoryWA(WAApi(urlWA, apiKeyWA)));
